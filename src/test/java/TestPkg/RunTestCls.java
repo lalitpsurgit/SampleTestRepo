b217809abcdc3859fbb998ab7f2b59c5947d9fef
lalitpsurgit <lalit.sur@gmail.com>
2026-04-25 22:13:22 +0530

    commit

--- src/test/java/TestPkg/RunTestCls.java
+++ src/test/java/TestPkg/RunTestCls.java
@@ -5,6 +5,7 @@ public class RunTestCls {
 	public static void main(String[] args) {
 		System.out.println("Test Rin from GIT");
 		System.out.println("Test Rin from GIT added newly");
+		System.out.println("Test Rin from GIT added newly newly");
 	}
 
 }
